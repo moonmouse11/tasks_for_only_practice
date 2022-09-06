f998c235b088fc9c1c0f231dcafc836ee4e1d5d7
moonmouse11 <70607402+moonmouse11@users.noreply.github.com>
2022-09-07 00:22:37 +0500

    Code style work

--- template.php
+++ template.php
@@ -12,28 +12,29 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-    <?if(! empty($arResult['ITEMS'])) :?> 
-    <div id="barba-wrapper">
-        <div class="article-list">
-        <?php foreach ($arResult['ITEMS'] as $arItem): ?>
-            <?
-                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-            ?>
-            <a class="article-item article-list__item" href="<? echo $arItem["DETAIL_PAGE_URL"] ?>"
-                                 data-anim="anim-3">
-            <div class="article-item__background"><img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
-                                                   data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx"
-                                                   alt="<?= $arItem["NAME"] ?>"/></div>
+<? if(! empty($arResult['ITEMS'])): ?> 
+<div id="barba-wrapper">
+    <div class="article-list">
+    <? foreach ($arResult['ITEMS'] as $arItem): ?>
+        <?
+            $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+            $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), 
+                                   array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+        ?>
+        <a class="article-item article-list__item" href="<? echo $arItem["DETAIL_PAGE_URL"] ?>" data-anim="anim-3">
+            <div class="article-item__background">
+                <img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>"
+                data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx"
+                alt="<?= $arItem["NAME"] ?>"/>
+            </div>
             <div class="article-item__wrapper">
                 <div class="article-item__title"><?= $arItem["NAME"] ?></div>
-                <div class="article-item__content"><?= $arItem["PREVIEW_TEXT"]; ?>
-                </div>
+                <div class="article-item__content"><?= $arItem["PREVIEW_TEXT"]; ?></div>
             </div>
         </a>
-        <? endforeach; ?>
-        </div>
+    <? endforeach; ?>
     </div>
-    <?php endif; ?>
+</div>
+<?php endif; ?>
 
 
