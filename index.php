dcd9078e6652be8e9f63c1c652a9d4ee5f68e1ad
moonmouse11 <70607402+moonmouse11@users.noreply.github.com>
2022-09-06 14:35:53 +0500

    Update, think it's right

--- index.php
+++ index.php
@@ -2,72 +2,10 @@
 define("HIDE_SIDEBAR", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 ?>
-	<div id="barba-wrapper">
-    <div class="article-list"><a class="article-item article-list__item" href="for-individuals.html"
-                                 data-anim="anim-3">
-        <div class="article-item__background"><img src="<?=SITE_TEMPLATE_PATH?>/assets/images/article-item-bg-6.jpg"
-                                                   data-src="xxxHTMLLINKxxx0.39186223192351520.41491856731872767xxx"
-                                                   alt=""/></div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title">Для физических лиц</div>
-            <div class="article-item__content">Лучшие решения для вашего дома: быстрый интернет, доступное кабельное&nbsp;TV,
-                удобный домашний телефон
-            </div>
-        </div>
-    </a><a class="article-item article-list__item" href="#" data-anim="anim-3">
-        <div class="article-item__background"><img src="<?=SITE_TEMPLATE_PATH?>/assets/images/article-item-bg-3.jpg"
-                                                   data-src="xxxHTMLLINKxxx0.153709056148504830.8920151245249737xxx"
-                                                   alt=""/></div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title">Средний и малый бизнес</div>
-            <div class="article-item__content">Быстро и&nbsp;качественно помогаем предпринимателям в&nbsp;решении
-                бизнес-задач
-            </div>
-        </div>
-    </a><a class="article-item article-list__item" href="for-state.html" data-anim="anim-3">
-        <div class="article-item__background"><img src="<?=SITE_TEMPLATE_PATH?>/assets/images/article-item-bg-4.jpg"
-                                                   data-src="xxxHTMLLINKxxx0.83331501539025420.9635873669140569xxx"
-                                                   alt=""/></div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title">Государственные заказчики</div>
-            <div class="article-item__content">Решения для государственных структур, повышение безопасности и&nbsp;комфорта
-                городской среды
-            </div>
-        </div>
-    </a><a class="article-item article-list__item" href="for-federals.html" data-anim="anim-3">
-        <div class="article-item__background"><img src="<?=SITE_TEMPLATE_PATH?>/assets/images/article-item-bg-5.jpg"
-                                                   data-src="xxxHTMLLINKxxx0.274858315149753230.570917169144997xxx"
-                                                   alt=""/></div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title">Федеральные клиенты</div>
-            <div class="article-item__content">Повышаем эффективность бизнес-процессов за&nbsp;счет внедрения
-                современных средств передачи и&nbsp;защиты данных
-            </div>
-        </div>
-    </a><a class="article-item article-list__item" href="for-telecommunications.html" data-anim="anim-3">
-        <div class="article-item__background"><img src="<?=SITE_TEMPLATE_PATH?>/assets/images/article-item-bg-2.jpg"
-                                                   data-src="xxxHTMLLINKxxx0.4314468597192560.505419651272456xxx"
-                                                   alt=""/></div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title">Операторы связи</div>
-            <div class="article-item__content">Предлагаем партнерство и&nbsp;взаимовыгодное сотрудничество</div>
-        </div>
-    </a><a class="article-item article-list__item" href="innovative-projects.html" data-anim="anim-3">
-        <div class="article-item__background"><img src="<?=SITE_TEMPLATE_PATH?>/assets/images/article-item-bg-1.jpg"
-                                                   data-src="xxxHTMLLINKxxx0.2544727135416540.7321213588928357xxx"
-                                                   alt=""/></div>
-        <div class="article-item__wrapper">
-            <div class="article-item__title">Инновационные проекты</div>
-            <div class="article-item__content">Предоставляем услуги широкополосного доступа в&nbsp;интернет и&nbsp;комплексные
-                решения на&nbsp;базе технологий промышленного интернета вещей (IoT)
-            </div>
-        </div>
-    </a></div>
-</div>
 <?$APPLICATION->SetTitle("Новости");?>
 <?$APPLICATION->IncludeComponent(
 	"bitrix:news.list", 
-	"bootstrap_v4", 
+	"maybe_final", 
 	array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "Y",
@@ -126,7 +64,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 		"SORT_ORDER1" => "DESC",
 		"SORT_ORDER2" => "ASC",
 		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "bootstrap_v4",
+		"COMPONENT_TEMPLATE" => "maybe_final",
 		"TEMPLATE_THEME" => "blue",
 		"MEDIA_PROPERTY" => "",
 		"SLIDER_PROPERTY" => "",
