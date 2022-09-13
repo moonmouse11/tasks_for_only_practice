c7cd760a0b03d8195c128281eb3e489eb736bb18
moonmouse11 <70607402+moonmouse11@users.noreply.github.com>
2022-09-13 14:25:11 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Форма");
-?>
-
-<?$APPLICATION->IncludeComponent("bitrix:form.result.new", "template", Array(
-	"SEF_MODE" => "Y",	// Включить поддержку ЧПУ
-		"WEB_FORM_ID" => "1",	// ID веб-формы
-		"LIST_URL" => "",	// Страница со списком результатов
-		"EDIT_URL" => "",	// Страница редактирования результата
-		"SUCCESS_URL" => "/succes.php",	// Страница с сообщением об успешной отправке
-		"CHAIN_ITEM_TEXT" => "",	// Название дополнительного пункта в навигационной цепочке
-		"CHAIN_ITEM_LINK" => "",	// Ссылка на дополнительном пункте в навигационной цепочке
-		"IGNORE_CUSTOM_TEMPLATE" => "Y",	// Игнорировать свой шаблон
-		"USE_EXTENDED_ERRORS" => "Y",	// Использовать расширенный вывод сообщений об ошибках
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CACHE_TIME" => "3600",	// Время кеширования (сек.)
-		"SEF_FOLDER" => "/",	// Каталог ЧПУ (относительно корня сайта)
-		"COMPONENT_TEMPLATE" => ".default"
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:form.result.new", 
+	"template", 
+	array(
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "A",
+		"CHAIN_ITEM_LINK" => "",
+		"CHAIN_ITEM_TEXT" => "",
+		"COMPONENT_TEMPLATE" => "template",
+		"EDIT_URL" => "",
+		"IGNORE_CUSTOM_TEMPLATE" => "N",
+		"LIST_URL" => "",
+		"SEF_FOLDER" => "/",
+		"SEF_MODE" => "Y",
+		"SUCCESS_URL" => "",
+		"USE_EXTENDED_ERRORS" => "Y",
+		"WEB_FORM_ID" => "3"
 	),
 	false
-);?>
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
