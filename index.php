7210291c941fdf4a062e512784ce7bb0782cc5c8
moonmouse11 <70607402+moonmouse11@users.noreply.github.com>
2022-10-04 14:21:13 +0500

    It's a parser

--- index.php
+++ index.php
@@ -1,76 +1,96 @@
 <?
-define("HIDE_SIDEBAR", true);
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-?>
-<?$APPLICATION->SetTitle("Новости");?>
-<?$APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"maybe_final", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => $_REQUEST["ID"],
-		"IBLOCK_TYPE" => "news",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "3",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"SET_BROWSER_TITLE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "Y",
-		"SET_META_KEYWORDS" => "Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "maybe_final",
-		"TEMPLATE_THEME" => "blue",
-		"MEDIA_PROPERTY" => "",
-		"SLIDER_PROPERTY" => "",
-		"SEARCH_PAGE" => "/search/",
-		"USE_RATING" => "N",
-		"USE_SHARE" => "N"
-	),
-	false
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+$APPLICATION->SetTitle("Интернет-магазин \"Парсер вакансий\"");
+
+CModule::IncludeModule('iblock');
+
+$IBLOCK_ID = 26;
+$el = new CIBlockElement;
+$arProps = [];
+
+$properties = CIBlockProperty::GetList(Array(), Array("ACTIVE" => "Y", "IBLOCK_ID" => $IBLOCK_ID));
+while ($prop_fields = $properties->GetNext()) {
+    if ($prop_fields["PROPERTY_TYPE"] !== "L") {
+        continue;
+    }
+    $property_enums = CIBlockPropertyEnum::GetList(Array(), Array("IBLOCK_ID" => $IBLOCK_ID, "CODE" => $prop_fields["CODE"]));
+    while ($enum_fields = $property_enums->GetNext()) {
+        $arProps[$prop_fields["CODE"]][$enum_fields["VALUE"]] = $enum_fields["ID"];
+    }
+}
+
+
+
+if (($file = fopen("res/vacancy.csv", "r")) !== false) {
+    $line = 1;
+    while (($data = fgetcsv($file, 1000, ",")) !== false) {
+        if ($line == 1) {
+            $line++;
+            continue;
+        }
+        $line++;
+
+        $PROP['ACTIVITY'] = $data[9];
+        $PROP['FIELD'] = $data[11];
+        $PROP['OFFICE'] = $data[1];
+        $PROP['LOCATION'] = $data[2];
+        $PROP['REQUIRE'] = explode("•", $data[4]);
+        $PROP['DUTY'] = explode("•", $data[5]);
+        $PROP['CONDITIONS'] = explode("•", $data[6]);
+        $PROP['EMAIL'] = $data[12];
+        $PROP['DATE'] = date('d.m.Y');
+        $PROP['TYPE'] = $data[8];
+        $PROP['SCHEDULE'] = $data[10];
+        $PROP['SALARY_TYPE'] = "";
+        $PROP['SALARY_VALUE'] = $data[7];
+
+        if ($PROP['SALARY_VALUE'] == '-' || !$PROP['SALARY_VALUE']) {
+            $PROP['SALARY_VALUE'] = '';
+        } elseif ($PROP['SALARY_VALUE'] == 'по договоренности') {
+            $PROP['SALARY_VALUE'] = '';
+            $PROP['SALARY_TYPE'] = $arProps['SALARY_TYPE']['договорная'];
+        } else {
+            $arSalary = explode(' ', $PROP['SALARY_VALUE']);
+            if ($arSalary[0] == 'от' || $arSalary[0] == 'до') {
+                $PROP['SALARY_TYPE'] = $arProps['SALARY_TYPE'][$arSalary[0]];
+                array_splice($arSalary, 0, 1);
+                $PROP['SALARY_VALUE'] = implode(' ', $arSalary);
+            } else {
+                $PROP['SALARY_TYPE'] = $arProps['SALARY_TYPE']['='];
+            }
+        }
+
+        foreach ($PROP as $key => $value) {
+            if (!$arProps[$key]) {
+                continue;
+            }
+
+            foreach ($arProps[$key] as $prop_key => $prop_value) {
+                $value = mb_strtolower(trim($value));
+                $prop_key = mb_strtolower(trim($prop_key));
+                if ($key === "LOCATION") {
+                    $prop_key = explode(",", $prop_key)[0];
+                }
+                if ($value == $prop_key) {
+                    $PROP[$key] = $prop_value;
+                    break;
+                }
+            }
+        }
+
+        $arLoadProductArray = array(
+            "MODIFIED_BY"    => $USER->GetID(),
+            "IBLOCK_SECTION_ID" => false,
+            "IBLOCK_ID"      => $IBLOCK_ID,
+            "PROPERTY_VALUES" => $PROP,
+            "NAME"           => $data[3],
+            "ACTIVE"         => end($data) ? 'Y' : 'N',
+        );
+
+        if ($PRODUCT_ID = $el->Add($arLoadProductArray))
+            echo "New ID: " . $PRODUCT_ID . "<br>";
+        else
+            echo "Error: " . $el->LAST_ERROR;
+    }
+    fclose($file);
+}
